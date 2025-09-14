e7f733f68aeb9506fb56e0b10f55c2fe6042f518
zhangxinjie <www.zhangxinjie@gmail.com>
2025-09-14 22:28:31 +0800

    feat: web server

--- main.go
+++ main.go
@@ -1,42 +1,22 @@
 package main
 
-import "fmt"
+import (
+	_ "first_golang/lib2"
+	"fmt"
+	"log"
+	"net/http"
+)
 
-// 返回单个值
-func fool(a string, b int) string {
-	fmt.Println("a:", a)
-	fmt.Println("b:", b)
-	return a
-}
-
-// 返回多个值
-func foo2(a string, b int) (string, int) {
-	fmt.Println("a: ", a)
-	fmt.Println("b: ", b)
-	return a, b
-}
-
-// 返回多个带名称的
-func foo3(a string, b int) (r1 string, r2 int) {
-	r1 = "1000"
-	r2 = 2000
-	return
-}
-
-func foo4() (r1, r2 int) {
-	r1 = 2
-	r2 = 4
-	return
-}
-
-// 二、函数
 func main() {
-	c := fool("hello", 666)
-	fmt.Println("c: ", c)
+	fmt.Printf("Server start...")
+	http.HandleFunc("/", handler)
+	if err := http.ListenAndServe("localhost:1234", nil); err != nil {
+		log.Fatal("ListenAndServe err: ", err)
+	}
+}
 
-	foo2("world", 888)
-	foo3("122", 122)
-
-	x, y := foo4()
-	fmt.Printf("x: %v\ty: %v\n", x, y)
+func handler(w http.ResponseWriter, r *http.Request) {
+	s := "hello world"
+	fmt.Fprintf(w, "%s", s)
+	log.Printf("%s", s)
 }
