e8a141e9e53cca7693cd5c53384edafc48a613df
zhangxinjie <www.zhangxinjie@gmail.com>
2025-09-14 21:28:20 +0800

    feat: function multi return

--- main.go
+++ main.go
@@ -2,27 +2,41 @@ package main
 
 import "fmt"
 
-// 定义枚举
-const (
-	BEIJING = iota
-	SHANGHAI
-	SHENZHEN
-)
+// 返回单个值
+func fool(a string, b int) string {
+	fmt.Println("a:", a)
+	fmt.Println("b:", b)
+	return a
+}
 
-const (
-	a, b = iota + 1, iota + 2
-	c, d
-	e, f
-)
+// 返回多个值
+func foo2(a string, b int) (string, int) {
+	fmt.Println("a: ", a)
+	fmt.Println("b: ", b)
+	return a, b
+}
 
-// 二、常量
+// 返回多个带名称的
+func foo3(a string, b int) (r1 string, r2 int) {
+	r1 = "1000"
+	r2 = 2000
+	return
+}
+
+func foo4() (r1, r2 int) {
+	r1 = 2
+	r2 = 4
+	return
+}
+
+// 二、函数
 func main() {
-	const len = 10
-	fmt.Printf("type of len: %T\n", len)
+	c := fool("hello", 666)
+	fmt.Println("c: ", c)
 
-	fmt.Printf("address: %d\n", SHANGHAI)
+	foo2("world", 888)
+	foo3("122", 122)
 
-	fmt.Println(a, b)
-	fmt.Println(c, d)
-	fmt.Println(e, f)
+	x, y := foo4()
+	fmt.Printf("x: %v\ty: %v\n", x, y)
 }
